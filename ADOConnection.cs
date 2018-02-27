0fce26d1598d764a0ef244c2b596dbe8241f75ad
mahmoudsallame <36882182+mahmoudsallame@users.noreply.github.com>
2018-02-27 17:41:14 +0200

    Add files via upload

---
+++ ADOConnection.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace App_Hospital.Models
+{
+    public class ADOConnection
+    {
+        //SqlConnection conn;
+        //SqlCommand comm;
+        //SqlDataReader dreader;
+        string connstring =ConfigurationManager.ConnectionStrings["ConnectionADO"].ConnectionString.ToString();
+        /// <summary>
+        /// Insert Patient Data
+        /// </summary>
+        /// <param name="FirstName"></param>
+        /// <param name="LastName"></param>
+        /// <param name="Address"></param>
+        /// <param name="DateOfBirth"></param>
+        /// <returns></returns>
+        public  bool RunInsert(string FirstName, string LastName, string Address, DateTime DateOfBirth)
+        {
+            //string result = "false";
+            StringBuilder sql = new StringBuilder("");
+            SqlCommand com = new SqlCommand();
+            sql.AppendFormat(@"insert into tbl_Patients(Patient_Fanme,Patient_Lname,Patient_Address,Patient_Date,Patient_InsertionDateTime)  ");
+            sql.AppendFormat(@"values(@fname,@lname,@address,@dateBirth,@insertionDate)");
+            com.CommandText = sql.ToString();
+            com.Parameters.AddWithValue("@fname",FirstName);
+            com.Parameters.AddWithValue("@lname",LastName);
+            com.Parameters.AddWithValue("@address",Address);
+            com.Parameters.AddWithValue("@dateBirth", DateOfBirth);
+            com.Parameters.AddWithValue("@insertionDate",DateTime.Now);
+            return  SaveToDB(com);
+                
+        }
+
+        /// <summary>
+        /// Using ADO Connection DataBase 
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        public static bool SaveToDB(SqlCommand command)
+        {
+            bool save = false;
+            string _constring = ConfigurationManager.ConnectionStrings["ConnectionADO"].ConnectionString.ToString(); ;
+            SqlConnection con = new SqlConnection(_constring);
+            command.Connection = con;
+            if (con.State==ConnectionState.Closed)
+            {
+                con.Open();
+                try
+                {
+                    if (command.ExecuteNonQuery() > 0)
+                    {
+                        save = true;
+                    }
+                }
+                catch (Exception)
+                {
+                    
+                    con.Close();
+                }
+                
+            }
+            return save;
+
+        }
+    }
+}
---
+++ PatientOperation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace App_Hospital.Models
+{
+    public class PatientOperation
+    {
+        HospitalDBEntities db = new HospitalDBEntities();
+        tbl_Patients patient;
+        ADOConnection adoConnection;
+        /// <summary>
+        /// Save Data EF OR ADO You Have Tow Options You Can Setting From Web Config
+        /// </summary>
+        /// <param name="FirstName"></param>
+        /// <param name="LastName"></param>
+        /// <param name="Address"></param>
+        /// <param name="DateOfBirth"></param>
+        /// <returns></returns>
+        public bool SavePatientData(string FirstName, string LastName, string Address, DateTime DateOfBirth)
+        {
+            bool Results = false;
+            string checkConnection = ConfigurationManager.AppSettings["CheckConnection"].ToString().ToUpper();
+            if (checkConnection == "ADO")
+            {
+                adoConnection = new ADOConnection();
+                Results=adoConnection.RunInsert(FirstName, LastName, Address, DateOfBirth);
+            }
+            else
+            {
+                patient = new tbl_Patients();
+                patient.Patient_Fanme = FirstName;
+                patient.Patient_Lname = LastName;
+                patient.Patient_Address = Address;
+                patient.Patient_Date = DateOfBirth;
+                patient.Patient_InsertionDateTime = DateTime.Now;
+                db.tbl_Patients.Add(patient);
+                int result = db.SaveChanges();
+                if (result > 0)
+                {
+                   Results=  true;
+                }
+                else
+                {
+                    Results= false;
+                }
+            }
+            return Results;
+        }
+    }
+}
